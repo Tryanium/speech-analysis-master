f87b7df0a2bd3167dde648521c8c52521203e9bb
marin.mouscadet <marin.mouscadet@ibm.com>
2019-09-12 15:13:59 +0200

    Changement CF

--- routes/ReadPDF.js
+++ routes/ReadPDF.js
@@ -3,11 +3,12 @@ var router = express.Router();
 
 /** PDF READER **/
 const fs = require('fs');
-const pdf = require('pdf-parse');
+//const pdf = require('pdf-parse');
 
 let dataBuffer = fs.readFileSync('docTEST.pdf');
 
 router.post('/', function (req, res) {
+  /*
   let pdfFile = req.body;
   console.log(pdfFile);
   pdf(dataBuffer).then(function(data) {
@@ -15,6 +16,8 @@ router.post('/', function (req, res) {
   }).catch(function(error){
     console.log(error);
   });
+  */
+  res.send("OKAY");
 });
 
 module.exports = router;
